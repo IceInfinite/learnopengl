0e8848f5a0cb54ecfac9818f401cf9854b79743c
Changbin Hong <infiniteice@foxmail.com>
2022-10-12 15:59:14 +0800

    feat: color batch render

--- learnopengl/res/shaders/basic.shader
+++ learnopengl/res/shaders/basic.shader
@@ -2,16 +2,18 @@
 #version 330 core
 
 layout(location = 0) in vec4 position;
-layout(location = 1) in vec2 tex_coord;
+layout(location = 1) in vec4 color;
+layout(location = 2) in vec2 tex_coord;
 
 uniform mat4 u_mvp;
 
 out vec2 v_tex_coord;
+out vec4 v_color;
 
-void main()
-{
-	gl_Position = u_mvp * position;
-	v_tex_coord = tex_coord;
+void main() {
+  gl_Position = u_mvp * position;
+  v_color = color;
+  v_tex_coord = tex_coord;
 };
 
 #shader fragment
@@ -20,13 +22,11 @@ void main()
 layout(location = 0) out vec4 color;
 
 in vec2 v_tex_coord;
+in vec4 v_color;
 
 uniform sampler2D u_texture0;
-//uniform sampler2D u_texture1;
 
-void main()
-{
-	vec4 tex_color = texture(u_texture0, v_tex_coord);
-	//tex_color += texture(u_texture1, v_tex_coord);
-	color = tex_color;
+void main() {
+  vec4 tex_color = texture(u_texture0, v_tex_coord);
+  color = v_color;
 };
--- learnopengl/src/test/texture_test.cc
+++ learnopengl/src/test/texture_test.cc
@@ -17,24 +17,38 @@ namespace test {
 TextureTest::TextureTest()
     : translation_a_(100.0f, 100.0f, 0.0f),
       translation_b_(200.0f, 200.0f, 0.0f) {
-  float positions[] = {
-      -50.0f, -50.0f, 0.0f, 0.0f,  // 0
-      50.0f,  -50.0f, 1.0f, 0.0f,  // 1
-      50.0f,  50.0f,  1.0f, 1.0f,  // 2
-      -50.0f, 50.0f,  0.0f, 1.0f   // 3
+  // clang-format off
+    float positions[] = {
+      -50.0f, -50.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f,  
+       50.0f, -50.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f,  
+       50.0f,  50.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f,  
+      -50.0f,  50.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 
+
+      100.0f, -50.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,  
+      200.0f, -50.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,  
+      200.0f,  50.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,  
+      100.0f,  50.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f
   };
 
-  unsigned int indices[] = {0, 1, 2, 2, 3, 0};
+  unsigned int indices[] = {
+      0, 1, 2, 
+      2, 3, 0,
+
+      4, 5, 6,
+      6, 7, 4
+  };
+  // clang-format on
 
   GLCall(glEnable(GL_BLEND));
   GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
   va_ = std::make_unique<VertexArray>();
-  vb_ = std::make_unique<VertexBuffer>(positions, 4 * 4 * sizeof(float));
-  ib_ = std::make_unique<IndexBuffer>(indices, 6);
+  vb_ = std::make_unique<VertexBuffer>(positions, 8 * 8 * sizeof(float));
+  ib_ = std::make_unique<IndexBuffer>(indices, 12);
 
   layout_ = std::make_unique<VertexBufferLayout>();
   layout_->Push<float>(2);
+  layout_->Push<float>(4);
   layout_->Push<float>(2);
   va_->AddLayout(*vb_, *layout_);
 
@@ -63,23 +77,16 @@ void TextureTest::OnRender() {
   shader_->Bind();
   texture0_->Bind();
   shader_->SetUniform1i("u_texture0", 0);
-  {
-    model_ = glm::translate(glm::mat4(1.0f), translation_a_);
-    glm::mat4 mvp = proj_ * view_ * model_;
-    shader_->SetUniformMat4("u_mvp", mvp);
-    Renderer::Draw(*va_, *ib_, *shader_);
-  }
 
-  {
-    model_ = glm::translate(glm::mat4(1.0f), translation_b_);
-    glm::mat4 mvp = proj_ * view_ * model_;
-    shader_->SetUniformMat4("u_mvp", mvp);
-    Renderer::Draw(*va_, *ib_, *shader_);
-  }
+  model_ = glm::translate(glm::mat4(1.0f), translation_a_);
+  glm::mat4 mvp = proj_ * view_ * model_;
+  shader_->SetUniformMat4("u_mvp", mvp);
+  Renderer::Draw(*va_, *ib_, *shader_);
 }
+
 void TextureTest::OnImGuiRender() {
   ImGui::SliderFloat3("TranslationA", &translation_a_.x, 0.0f, 960.0f);
-  ImGui::SliderFloat3("TranslationB", &translation_b_.x, 0.0f, 960.0f);
+  // ImGui::SliderFloat3("TranslationB", &translation_b_.x, 0.0f, 960.0f);
   ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
               1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
 }
